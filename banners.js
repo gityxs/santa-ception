e53b332b96533d4d94d93f8a9db63ff9fcd5186b
zhaolinxu <zhaolinxu@sina.cn>
2024-12-23 14:28:22 +0800

    ym

---
+++ banners.js
@@ -0,0 +1,21 @@
+//banners
+var banners = {
+//	fushioncash :'<a href="http://www.mb01.com/lnk.asp?o=2575&c=36123&a=132341"><IMG SRC="http://www.mb01.com/getimage.asp?a=132341&m=1854&o=2575&i=36123.dat" width=120 height=600 border=0></a>',
+//	freeiphone6 :'<a href="http://www.mb103.com/lnk.asp?o=7072&c=78514&a=132341"><IMG SRC="http://www.mb103.com/getimage.asp?a=132341&m=79&o=7072&i=78514.dat" width=120 height=600 border=0></a>',
+//	leagueofangels :'<a href="http://www.mb103.com/lnk.asp?o=6531&c=73000&a=132341"><IMG SRC="http://www.mb103.com/getimage.asp?a=132341&m=2825&o=6531&i=73000.dat" width=160 height=600 border=0></a>',
+//	sitey: '<a href="http://www.mb103.com/lnk.asp?o=7068&c=79304&a=132341"><IMG SRC="http://www.mb103.com/getimage.asp?a=132341&m=3111&o=7068&i=79304.dat" width=160 height=600 border=0></a>',
+//	SurveyJunkie: '<a href="http://www.mb104.com/lnk.asp?o=5358&c=60999&a=132341"><IMG SRC="http://www.mb104.com/getimage.asp?a=132341&m=2889&o=5358&i=60999.dat" width=160 height=600 border=0></a>',
+//	mcafee: '<a href="http://www.mb57.com/lnk.asp?o=5675&c=65008&a=132341"><IMG SRC="http://www.mb57.com/getimage.asp?a=132341&m=2935&o=5675&i=65008.dat" width=160 height=600 border=0></a>',
+//	pccleaner: '<a href="http://www.mb103.com/lnk.asp?o=7191&c=80989&a=132341"><IMG SRC="http://www.mb103.com/getimage.asp?a=132341&m=2710&o=7191&i=80989.dat" width=160 height=600 border=0></a>',
+//	starwarsgame: '<a href="http://www.mb103.com/lnk.asp?o=5412&c=61901&a=132341"><IMG SRC="http://www.mb103.com/getimage.asp?a=132341&m=2825&o=5412&i=61901.dat" width=160 height=600 border=0></a>',
+//	royalquest: '<a href="http://www.mb103.com/lnk.asp?o=7357&c=81292&a=132341"><IMG SRC="http://www.mb103.com/getimage.asp?a=132341&m=2952&o=7357&i=81292.dat" width=160 height=600 border=0></a>',
+//	strongholdkingdoms: '<a href="http://www.mb103.com/lnk.asp?o=6446&c=72193&a=132341"><IMG SRC="http://www.mb103.com/getimage.asp?a=132341&m=2952&o=6446&i=72193.dat" width=160 height=600 border=0></a>',
+//	marvelheroes: '<a href="http://www.mb104.com/lnk.asp?o=5771&c=65687&a=132341"><IMG SRC="http://www.mb104.com/getimage.asp?a=132341&m=2825&o=5771&i=65687.dat" width=160 height=600 border=0></a>',
+//	harrispoll: '<a href="http://www.mb103.com/lnk.asp?o=5468&c=62687&a=132341"><IMG SRC="http://www.mb103.com/getimage.asp?a=132341&m=1802&o=5468&i=62687.dat" width=160 height=600 border=0></a>',
+//	globaltestmarket: '<a href="http://www.mb01.com/lnk.asp?o=3348&c=43374&a=132341"><IMG SRC="http://www.mb01.com/getimage.asp?a=132341&m=2414&o=3348&i=43374.dat" width=160 height=600 border=0></a>',
+//	euromillionairesystem: '<a href="http://www.mb38.com/lnk.asp?o=7359&c=81861&a=132341"><IMG SRC="http://www.mb38.com/getimage.asp?a=132341&m=3095&o=7359&i=81861.dat" width=120 height=600 border=0></a>',
+//	videostripe: '<a href="http://www.mb104.com/lnk.asp?o=6981&c=77832&a=132341"><IMG SRC="http://www.mb104.com/getimage.asp?a=132341&m=3094&o=6981&i=77832.dat" width=120 height=600 border=0></a>',
+//	agoda: '<a href="http://www.mb103.com/lnk.asp?o=7109&c=80887&a=132341"><IMG SRC="http://www.mb103.com/getimage.asp?a=132341&m=2917&o=7109&i=80887.dat" width=160 height=600 border=0></a>',
+//	pureasiangarcinia: '<a href="http://www.mb103.com/lnk.asp?o=6712&c=74916&a=132341"><IMG SRC="http://www.mb103.com/getimage.asp?a=132341&m=3042&o=6712&i=74916.dat" width=160 height=600 border=0></a>',
+	sexgansters: '<a href="http://www.sexgangsters.com/?pid=857" title=""><img alt="" src="http://landcdn.sexgangsters.com/revenue/b/non-adult/en/160x600/001.gif" style="height: 600px; width: 160px;" /></a>',
+}
